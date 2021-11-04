c678dace2413c7a98d67943054b428668a48ccc6
Mark <mark@example.dev>
2021-11-03 19:38:03 -0700

    further study 1

--- src/EightBall.js
+++ src/EightBall.js
@@ -4,17 +4,32 @@ import './EightBall.css';
 const EightBall = (props) => {
     const [color, setColor] = useState('black');
     const [text, setText] = useState('Think of a question.');
+    const [resCount, setResCount] = useState({green: 0, goldenrod: 0, red: 0});
 
     const displayAnswer = () => {
-        const randAns = props.answers[Math.floor(props.answers.length * Math.random())];
-        setColor(randAns.color);
-        setText(randAns.msg);
+        const {color, msg} = props.answers[Math.floor(props.answers.length * Math.random())];
+        setColor(color);
+        setText(msg);
+        const updateCount = {...resCount};
+        updateCount[color] += 1;
+        setResCount(updateCount);
+    }
+
+    const reset = () => {
+        setColor('black');
+        setText('Think of a question.');
+        setResCount({green: 0, goldenrod: 0, red: 0});  
     }
 
     return (
-    <div onClick={displayAnswer} className="EightBall-container" style={{backgroundColor: color}}>
-        <p>{text}</p>
-    </div>)
+    <div className="EightBall-container">
+        <div onClick={displayAnswer} className="EightBall-circle" style={{backgroundColor: color}}>
+            <p>{text}</p>
+        </div>
+        <button onClick={reset}>Reset</button>
+        <p>Yes: {resCount.green} No: {resCount.red} Uncertain: {resCount.goldenrod}</p>
+    </div>
+    )
 }
 
 export default EightBall;
